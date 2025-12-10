be12592b76d660fc99989634655fb68dc46c6bc3
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-12-10 22:25:11 +0700

    chore : setup DOM testing using jest with pure DOM

--- fundamentals/index.js
+++ fundamentals/index.js
@@ -20,14 +20,16 @@ function test(description, callback) {
 
 test("sum work as expected", () => {
   const sum = (a, b) => a + b;
-  let expectation = 96;
   let reality = sum(90, 6);
+  let expectation = 96;
+
   expect(reality).toBe(expectation);
 });
 
 test("substract work as expected", () => {
   const substract = (a, b) => a - b;
-  let expectation = 84;
   let reality = substract(90, 6);
+  let expectation = 84;
+
   expect(reality).toBe(expectation);
 });
