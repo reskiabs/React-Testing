0768f80689a5e3d1381899c1981b6583ea45d408
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2026-02-06 20:18:13 +0700

    updated folder structure

---
+++ testing/src/__tests__/Counter.test.js
@@ -0,0 +1,17 @@
+/** @jest-environment jsdom */
+
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Counter from "../components/Counter";
+
+test("survive refactoring", async () => {
+  const user = userEvent.setup();
+  render(<Counter />);
+  const button = screen.getByRole("button", { name: /count: 0/i });
+  expect(button).toBeInTheDocument();
+
+  await user.click(button);
+  // expect(button).toHaveTextContent("Count: 1");
+  expect(screen.getByRole("button", { name: /count: 1/i })).toBeInTheDocument();
+});
