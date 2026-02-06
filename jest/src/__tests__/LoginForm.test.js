573fe07b60498863a0818e27f7df684afb5b8470
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2026-02-06 20:13:51 +0700

    update strucured folder and prepare to login form testing

---
+++ jest/src/__tests__/LoginForm.test.js
@@ -0,0 +1,9 @@
+describe("Login Form Component", () => {
+  test("should render login form correctly", () => {
+    // Test implementation goes here
+  });
+
+  test("should handle form submission", () => {
+    // Test implementation goes here
+  });
+});
